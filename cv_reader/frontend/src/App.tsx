ca9b821f39ca27ec66da42d5c3d9c72f1f8d4bc4
FraanMoore <fraanmoore@users.noreply.github.com>
2026-03-24 15:49:17 -0300

    add react router dom

--- cv_reader/frontend/src/App.tsx
+++ cv_reader/frontend/src/App.tsx
@@ -1,14 +1,20 @@
-
-import './App.css'
-import Home from './components/Home'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import EditCV from './components/EditCV';
+import Home from './components/Home';
+import RootLayout from './utils/RootLayout';
 
 function App() {
-
   return (
-    <>
-      <Home />
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route element={<RootLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/edit-cv" element={<EditCV />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App
